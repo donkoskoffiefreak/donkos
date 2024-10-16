738f6f7bcbb563e04bbac43aba260bbe4c007ad7
donkoskoffiefreak <ferre.casteleyn@student.tersterre.be>
2024-10-16 09:14:54 +0200

    louissatan

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+var clicked = 0;
+
 function changeText(id){
- document.getElementById(id).textContent = "do not click on things";
+    clicked = clicked+1;
+
+     if (clicked >= 3){
+        console.log("in 3", id);
+        document.getElementById(id).textContent = "I WILL SENT DONKOS HITMEN IF YOU DON'T STOP";
+        document.getElementById(id).style.fontSize = "100px";
+        document.getElementById(id).style.color = "red";
+    } if (clicked >= 5){
+        console.log("in 4", id);
+        document.getElementById(id).textContent = "hitmen are sent to your location please stay at your location";
+        document.getElementById(id).style.fontSize = "200px";
+        document.getElementById(id).style.color = "black";
+    } if (clicked <= 2) {
+        console.log("in else", id);
+        document.getElementById(id).textContent = "do not click on things";
+    }
 }
 function changeImg(id){
     document.getElementById(id).src="img/hq720.jpg";
